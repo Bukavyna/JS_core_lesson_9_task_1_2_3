b6c5c65360c18d2b52a6c71cbc7ab9561d8ca80a
Taras <luchakt2@gmail.com>
2023-10-12 03:37:49 +0300

    area of a circle

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,54 @@
 "use strict";
 
+///////// Вирахувати площу кола !
+/*
+function areaCircle(r) {
+
+	if (r == ' ') {
+		alert(`Будь ласка, введіть радіус.`);
+		//document.write(`Будь ласка, введіть радіус.`);
+		//console.log(`Будь ласка, введіть радіус.`);
+	}
+
+	else if (isNaN(r) || +r <=0) {
+		//alert(`Повинно бути числове значення.`);
+		document.write(`Повинно бути числове значення.`);
+		//console.log(`Повинно бути числове значення.`);
+	}
+
+	 else {
+		let area = Math.PI * r * r;
+		//alert(`Площа доршвнює ${area} квадратних одиниць.`);
+		//document.write(`Площа дорівнює ${area} квадратних одиниць.`);
+		console.log(`Площа дорівнює ${area} квадратних одиниць.`);
+	}
+
+}
+
+let r = +prompt(`Задайте радіус кола.`);
+areaCircle(r);
+*/
+
+/*
+function areaCircle(r) {
+
+	if (r == ` `) {
+		return `Будь ласка, введіть радіус.`;
+	}
+
+	else if (isNaN(r) || r <=0) {
+		return (`Повинно бути числове значення.`);
+	}
+
+	else {
+		let area = Math.PI * r * r;
+		return (`Площа дорівнює ${area} квадратних одиниць.`);
+	}
+
+}
+
+console.log(areaCircle(2));
+console.log(areaCircle(` `));
+console.log(areaCircle(`sfkgen`));
+*/
+
