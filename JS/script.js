4f02551e5383ac715a0982dafd201a07e6efb221
Taras <luchakt2@gmail.com>
2023-10-12 19:48:58 +0300

    add squareRoot2

--- JS/script.js
+++ JS/script.js
@@ -58,34 +58,33 @@ console.log(areaCircle(`sfkgen`));
 function getSqrt(number) {
 
 	if (isNaN(number)) {
-		return `Повинно бути числове значення.`;
-		//document.write(`Повинно бути числове значення.`);
+		//return `Повинно бути числове значення.`;
+		document.write(`Повинно бути числове значення.`);
 	}
 
-	else if (number == ' ') {
-		return `Будь ласка, введіть число.`;
-		//document.write(`Будь ласка, введіть число.`);
+	else if (number ==  ` `) {
+		//return `Будь ласка, введіть число.`;
+		document.write(`Будь ласка, введіть число.`);
+		//Чому при введенні через "prompt" і відображенні через "document", вискакує: Введіть додатнє число.?
 	}
 
-	else if (number <= 0) {
-		return `Введіть додатнє число.`;
-		//document.write(`Введіть додатнє число.`);
+	else if (number < 0) {
+		//return `Введіть додатнє число.`;
+		document.write(`Введіть додатнє число.`);
 	}
 
 	else {
 		let squareRoot = Math.sqrt (number);
-		return `Квадратний корінь з ${number} дорівнює ${squareRoot}.`;
-		//document.write(`Квадратний корінь з ${number} дорівнює ${squareRoot}.`);
+		//return `Квадратний корінь з ${number} дорівнює ${squareRoot}.`;
+		document.write(`Квадратний корінь з ${number} дорівнює ${squareRoot}.`);
 	}
 
 }
 
-//let number = +prompt(`Введіть число.`);
-//getSqrt(number);
-console.log(getSqrt(2));
-console.log(getSqrt(` `));
-console.log(getSqrt(-6));
-console.log(getSqrt(`shenne`));
+let number = +prompt(`Введіть число.`);
+getSqrt(number);
+//console.log(getSqrt(`shenne`));
+//console.log(getSqrt(` `));
+//console.log(getSqrt(0));
+//console.log(getSqrt(2));
 */
-
-//Як зробити так, щоб коли ввів 0 не вискакувало 'Будь ласка, введіть число.'? Пробую по всякому, і не виходить. Дякую за допомогу!
