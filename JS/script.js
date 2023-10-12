e298345e6f7fc27ff28d5f2d63b9a2fc7ccb3c7a
Taras <luchakt2@gmail.com>
2023-10-12 05:06:51 +0300

    add squareRoot

--- JS/script.js
+++ JS/script.js
@@ -52,3 +52,40 @@ console.log(areaCircle(` `));
 console.log(areaCircle(`sfkgen`));
 */
 
+
+///////// Корінь квадратний !!
+/*
+function getSqrt(number) {
+
+	if (isNaN(number)) {
+		return `Повинно бути числове значення.`;
+		//document.write(`Повинно бути числове значення.`);
+	}
+
+	else if (number == ' ') {
+		return `Будь ласка, введіть число.`;
+		//document.write(`Будь ласка, введіть число.`);
+	}
+
+	else if (number <= 0) {
+		return `Введіть додатнє число.`;
+		//document.write(`Введіть додатнє число.`);
+	}
+
+	else {
+		let squareRoot = Math.sqrt (number);
+		return `Квадратний корінь з ${number} дорівнює ${squareRoot}.`;
+		//document.write(`Квадратний корінь з ${number} дорівнює ${squareRoot}.`);
+	}
+
+}
+
+//let number = +prompt(`Введіть число.`);
+//getSqrt(number);
+console.log(getSqrt(2));
+console.log(getSqrt(` `));
+console.log(getSqrt(-6));
+console.log(getSqrt(`shenne`));
+*/
+
+//Як зробити так, щоб коли ввів 0 не вискакувало 'Будь ласка, введіть число.'? Пробую по всякому, і не виходить. Дякую за допомогу!
